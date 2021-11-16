bf57972e89a7a67e37bc3002b1f4602a6f78531d
callanbr <chrisallan1992@icloud.com>
2021-11-16 12:45:26 -0700

    name changes and added lesson 7

---
+++ angular-l7-starter/src/app/app.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { GreetingComponent } from './shared/components/greeting/greeting.component';
+import { FormsModule } from '@angular/forms';
+import { MessageFormComponent } from './shared/components/message-form/message-form.component';
+import { SkillsModule } from './modules/skills/skills.module';
+
+@NgModule({
+  declarations: [AppComponent, GreetingComponent, MessageFormComponent],
+  imports: [BrowserModule, FormsModule, SkillsModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
